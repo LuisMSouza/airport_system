3ac007eda35cac5fac80dcb09caadcbd265ebe46
LuisMSouza <l.miguel-souza@hotmail.com>
2025-04-28 15:22:27 -0300

    Update ALLOWED_HOSTS in production settings for correct deployment URL

--- airport_sistem/settings/production.py
+++ airport_sistem/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 import dj_database_url
 
 DEBUG = False
-ALLOWED_HOSTS = ["airportsistem-eaa1fefe93b7.herokuapp.com"]
+ALLOWED_HOSTS = ["airportsystem-82ef0b028080.herokuapp.com"]
 
 DATABASES = {"default": dj_database_url.config(conn_max_age=600, ssl_require=True)}
 
